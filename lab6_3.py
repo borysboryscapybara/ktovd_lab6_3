3ddf89ac50d18283481c5244d647f44d89d74b37
Borys <wushatneen@gmail.com>
2024-11-08 19:14:06 +0200

    integrate data filtering and truncation and add progressbar

--- lab6_3.py
+++ lab6_3.py
@@ -1,60 +1,60 @@
 #pip install pandas scikit-learn numpy surprise
+#pip install numpy<2
 
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from surprise import Dataset, Reader
-from surprise import SVD
+from sklearn.decomposition import TruncatedSVD
+from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
+from tqdm import tqdm
 
-#
+# Завантаження даних
+print("Завантаження даних...")
 hotel_details = pd.read_csv("hotel/Hotel_details.csv")
 room_attributes = pd.read_csv("hotel/Hotel_Room_attributes.csv")
 price_data = pd.read_csv("hotel/hotel_price_min_max - Formula.csv")
 
-# Очищення даних: заповнення пропусків
+# Очищення даних
+print("Очищення даних...")
 hotel_details.fillna("", inplace=True)
 room_attributes.fillna("", inplace=True)
 price_data.fillna(0, inplace=True)
 
-# Об’єднання даних готелів та атрибутів кімнат
+# Об’єднання даних
+print("Об’єднання даних...")
 hotel_data = pd.merge(hotel_details, room_attributes, left_on="hotelid", right_on="hotelcode", how="inner")
-
-# Об’єднання даних з цінами
 hotel_data = pd.merge(hotel_data, price_data, on="hotelcode", how="left")
 
-# Токенізація текстових атрибутів, зручностей та описів
+# Об’єднання текстових ознак
+print("Створення текстових ознак...")
 hotel_data['combined_features'] = hotel_data['roomamenities'] + " " + hotel_data['roomtype'] + " " + hotel_data['propertytype']
 
-# Використання TF-IDF для перетворення текстових ознак у числові
+# Використання TF-IDF
+print("Побудова TF-IDF матриці...")
 tfidf = TfidfVectorizer(stop_words="english")
 tfidf_matrix = tfidf.fit_transform(hotel_data['combined_features'])
 
-# Обчислення косинусної схожості між готелями
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+# Зменшення розмірності
+print("Зменшення розмірності...")
+svd = TruncatedSVD(n_components=100)
+tfidf_reduced = svd.fit_transform(tfidf_matrix)
 
-# Створення функції для рекомендації готелів
+# Обчислення косинусної схожості
+print("Обчислення косинусної схожості...")
+cosine_sim = cosine_similarity(tfidf_reduced, tfidf_reduced)
+
+# Функція для рекомендацій готелів на основі схожості
 def get_recommendations(hotel_id, cosine_sim=cosine_sim):
-    # Знаходимо індекс готелю
     idx = hotel_data[hotel_data['hotelid'] == hotel_id].index[0]
-
-    # Отримуємо список схожості з іншими готелями
     sim_scores = list(enumerate(cosine_sim[idx]))
-
-    # Сортуємо готелі за схожістю
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Вибираємо 5 найсхожіших готелів
-    sim_scores = sim_scores[1:6]
-
-    # Отримуємо індекси готелів
+    sim_scores = sim_scores[1:6]  # Вибираємо 5 найсхожіших готелів
     hotel_indices = [i[0] for i in sim_scores]
-
-    # Повертаємо топ-5 рекомендованих готелів
     return hotel_data.iloc[hotel_indices][['hotelname', 'city', 'country', 'starrating', 'min', 'max']]
 
-# Приклад даних про рейтинги (імітовані)
+# Приклад даних про рейтинги
 ratings_dict = {
     "user_id": [1, 2, 3, 4, 5, 1, 2, 3],
     "hotel_id": [101, 102, 103, 104, 105, 102, 103, 104],
@@ -62,37 +62,45 @@ ratings_dict = {
 }
 ratings = pd.DataFrame(ratings_dict)
 
-# Створення dataset для Surprise
+# Створення датасету для Surprise
+print("Підготовка даних для колаборативної фільтрації...")
 reader = Reader(rating_scale=(1, 5))
 data = Dataset.load_from_df(ratings[["user_id", "hotel_id", "rating"]], reader)
 
 # Розділення на тренувальний і тестовий набори
+print("Розділення даних на тренувальні та тестові...")
 trainset, testset = train_test_split(data, test_size=0.2)
 
-# Використання моделі SVD для рекомендацій
+# Модель SVD для рекомендацій
+print("Навчання моделі SVD...")
 svd = SVD()
 svd.fit(trainset)
 
-# Оцінка моделі на тестовому наборі
+# Оцінка моделі
+print("Оцінка моделі...")
 predictions = svd.test(testset)
 
-
+# Функція для рекомендацій готелів для користувача
 def recommend_for_user(user_id, n_recommendations=5):
-    # Пошук всіх готелів, які не були оцінені користувачем
     hotels = hotel_data['hotelid'].unique()
     user_rated_hotels = ratings[ratings['user_id'] == user_id]['hotel_id']
     hotels_to_rate = [hotel for hotel in hotels if hotel not in user_rated_hotels.values]
 
-    # Оцінка та сортування за прогнозованим рейтингом
-    predictions = [svd.predict(user_id, hotel) for hotel in hotels_to_rate]
-    recommendations = sorted(predictions, key=lambda x: x.est, reverse=True)[:n_recommendations]
+    # Виведення прогрес-бару
+    print(f"Генерація рекомендацій для користувача {user_id}...")
+    recommendations = []
+    for hotel in tqdm(hotels_to_rate, desc="Processing"):
+        pred = svd.predict(user_id, hotel)
+        recommendations.append(pred)
 
-    # Повертаємо рекомендовані готелі
+    recommendations = sorted(recommendations, key=lambda x: x.est, reverse=True)[:n_recommendations]
     recommended_hotel_ids = [pred.iid for pred in recommendations]
     return hotel_data[hotel_data['hotelid'].isin(recommended_hotel_ids)][['hotelname', 'city', 'starrating', 'min', 'max']]
 
 # Рекомендації для довільного готелю на основі контентно-орієнтованої моделі
+print("\nРекомендації для готелю:")
 print(get_recommendations(177167))
 
 # Рекомендації для користувача на основі колаборативної фільтрації
+print("\nРекомендації для користувача:")
 print(recommend_for_user(user_id=1))
